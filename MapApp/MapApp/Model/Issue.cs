9102452513cc3552b4fc86c763301fd3b68cc9f3
MikoloD <grygiel.mikolaj@gmail.com>
2021-08-05 12:24:29 +0200

    Create Issue.cs

---
+++ MapApp/MapApp/Model/Issue.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MapApp.Model
+{
+    public class Issue : DbContext
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public byte[] Blob { get; set; }
+    }
+}
