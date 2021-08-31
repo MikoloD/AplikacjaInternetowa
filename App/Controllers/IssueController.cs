1acfb7735156ee72e27e4832e9e47f73d3f96c07
MikoloD <grygiel.mikolaj@gmail.com>
2021-08-31 12:35:57 +0200

    commit

--- App/Controllers/IssueController.cs
+++ App/Controllers/IssueController.cs
@@ -8,15 +8,21 @@ using App.Models;
 using Microsoft.EntityFrameworkCore;
 using System.Buffers.Text;
 using System.Text.RegularExpressions;
+using System.Net.Mail;
+using System.Net;
+using Microsoft.Extensions.Configuration;
+using System.Text;
 
 namespace App.Controllers
 {
     public class IssueController : Controller
     {
         private readonly ApplicationDbContext _context;
-        public IssueController(ApplicationDbContext context)
+        private readonly IConfiguration _configuration;
+        public IssueController(ApplicationDbContext context, IConfiguration configuration)
         {
             _context = context;
+            _configuration = configuration;
         }
         [HttpGet]
         public IActionResult Issue()
@@ -38,7 +44,39 @@ namespace App.Controllers
             issue.State = State.Reported;
             _context.Issues.Add(issue);
             _context.SaveChanges();
+
+            var smtpClient = new SmtpClient("172.16.35.195")
+            {
+                Port = 25,
+                //Credentials = new NetworkCredential("email", "password"),
+                EnableSsl = false,
+            };
+            string photos = PhotoLinks(issue.Images);
+            var mailMessage = new MailMessage
+            {
+                From = new MailAddress(_configuration.GetSection("NotificationMailFrom").Value),
+                Subject = "Zgłoszenie awarii: "+issue.IssueId,
+                Body = "<p>"+issue.Description+"</p>"+"Zdjęcia: <br>"+ photos,
+                IsBodyHtml = true,
+            };
+            mailMessage.To.Add(_configuration.GetSection("NotificationMailTo").Value);
+
+            smtpClient.Send(mailMessage);
+
             return RedirectToAction("Index","Home");
+        }
+        public string PhotoLinks(List<Multimedium> PhotoLinks)
+        {
+            StringBuilder result = new StringBuilder();
+            foreach (var item in PhotoLinks)
+            {
+                if (item.Image != null)
+                {
+                    result.AppendLine(
+                        "<a href = '" + _configuration.GetSection("HostURL").Value + "get.image?img=" + item.MultimediumId.ToString() + "'>zdjęci-" + item.MultimediumId.ToString() + "</a><br>");
+                }
+            }
+            return result.ToString();
         }
     }
 }
--- App/Startup.cs
+++ App/Startup.cs
@@ -40,6 +40,7 @@ namespace App
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
+            services.AddSingleton<IConfiguration>(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
